1d8f3c8895d2adfba17c57041ade9419110e9802
Davy Campano <davy@spicerackmedia.com>
2018-04-08 22:03:28 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,8 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
-                <h2>Davy NEW APP</h2>
+                <h2>MAKING ANOTHER CHANGE!</h2>
+                
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
